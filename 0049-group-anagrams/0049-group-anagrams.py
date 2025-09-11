c67d932bd7c02b56925ea8848cf331ab58566fbb
Vamsi Chinnam <vamsichinnam152002@gmail.com>
2025-09-11 20:13:53 +0530

    12 ms (60.23%) 20.7 MB (62.72%) 09-11-2025 0049-group-anagrams Medium .py

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -6,3 +6,13 @@ class Solution:
             key = ''.join(sorted(w))
             groups[key].append(w)
         return list(groups.values())
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         res = defaultdict(list)
+
+#         for s in strs:
+#             count = [0] * 26
+#             for c in s:
+#                 count[ord(c) - ord('a')] +=1
+#             res[tuple(count)].append(s)
+#         return list(res.values())
